db89c78487975c726a02a6c9c21975f83e859a3a
WJConnors <37446973+WJConnors@users.noreply.github.com>
2024-10-08 10:24:14 +0100

    Added basic sim loop

--- GameOfLife/GameOfLife/GameOfLife.cpp
+++ GameOfLife/GameOfLife/GameOfLife.cpp
@@ -7,7 +7,21 @@ int main()
 {
 	Board board = Board();
 	board.displayBoard();
-	board.updateBoard();
-	cout << endl;
-	board.displayBoard();
+	bool sim = true;
+	while (sim) {
+		cout << "Choose an option:" << endl;
+		cout << "1. Step forward" << endl;
+		cout << "2. End simulation" << endl;
+		int in;
+		cin >> in;
+
+		switch (in) {
+		case 1:
+			board.updateBoard();
+			board.displayBoard();
+			break;
+		case 2:
+			sim = false;
+		}
+	}
 }
