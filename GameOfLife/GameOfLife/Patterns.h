cc82c5fe7a2d1a9c13bdfdda73f92558262a5424
WJConnors <37446973+WJConnors@users.noreply.github.com>
2024-10-14 12:19:16 +0100

    Updated patterns store before modifying logic to include edges

--- GameOfLife/GameOfLife/Patterns.h
+++ GameOfLife/GameOfLife/Patterns.h
@@ -4,89 +4,71 @@
 namespace Patterns
 {
 	//============Still Lifes===================
-	constexpr int block_x_size{ 4 };
-	constexpr int block_y_size{ 4 };
-	constexpr int block[16]{
-	0,0,0,0,
-	0,1,1,0,
-	0,1,1,0,
-	0,0,0,0
+	constexpr int block_x_size{ 2 };
+	constexpr int block_y_size{ 2 };
+	constexpr int block[4]{
+	1,1,
+	1,1
 	};
 
-	constexpr int beehive1_x_size{ 5 };
-	constexpr int beehive1_y_size{ 6 };
-	constexpr int beehive1[30]{
-	 0,0,0,0,0,0,
-	 0,0,1,1,0,0,
-	 0,1,0,0,1,0,
-	 0,0,1,1,0,0,
-	 0,0,0,0,0,0
+	constexpr int beehive1_x_size{ 3 };
+	constexpr int beehive1_y_size{ 4 };
+	constexpr int beehive1[12]{
+	 0,1,1,0,
+	 1,0,0,1,
+	 0,1,1,0
 	};
 
-	constexpr int beehive2_x_size{ 6 };
-	constexpr int beehive2_y_size{ 5 };
-	constexpr int beehive2[30]{
-		0,0,0,0,0,
-		0,0,1,0,0,
-		0,1,0,1,0,
-		0,1,0,1,0,
-		0,0,1,0,0,
-		0,0,0,0,0
+	constexpr int beehive2_x_size{ 4 };
+	constexpr int beehive2_y_size{ 3 };
+	constexpr int beehive2[12]{
+		0,1,0,
+		1,0,1,
+		1,0,1,
+		0,1,0
 	};
 
 	//============Oscillators===================
-	constexpr int blinker_size{ 5 };
-	constexpr int blinker1[25]{
-		0,0,0,0,0,
-		0,0,0,0,0,
-		0,1,1,1,0,
-		0,0,0,0,0,
-		0,0,0,0,0
+	constexpr int blinker_size{ 3 };
+	constexpr int blinker1[9]{
+		0,0,0,
+		1,1,1,
+		0,0,0
 	};
 
-	constexpr int blinker2[25]{
-		0,0,0,0,0,
-		0,0,1,0,0,
-		0,0,1,0,0,
-		0,0,1,0,0,
-		0,0,0,0,0
+	constexpr int blinker2[9]{
+		0,1,0,
+		0,1,0,
+		0,1,0
 	};
 
-	constexpr int toad_size{ 6 };
-	constexpr int toad1_1[36]{
-		0,0,0,0,0,0,
-		0,0,0,0,0,0,
-		0,0,1,1,1,0,
-		0,1,1,1,0,0,
-		0,0,0,0,0,0,
-		0,0,0,0,0,0
+	constexpr int toad_size{ 4 };
+	constexpr int toad1_1[16]{
+		0,0,0,0,
+		0,1,1,1,
+		1,1,1,0,
+		0,0,0,0
 	};
 
-	constexpr int toad1_2[36]{
-		0,0,0,0,0,0,
-		0,0,0,1,0,0,
-		0,1,0,0,1,0,
-		0,1,0,0,1,0,
-		0,0,1,0,0,0,
-		0,0,0,0,0,0
+	constexpr int toad1_2[16]{
+		0,0,1,0,
+		1,0,0,1,
+		1,0,0,1,
+		0,1,0,0
 	};
 
-	constexpr int toad2_1[36]{
-		0,0,0,0,0,0,
-		0,0,1,0,0,0,
-		0,0,1,1,0,0,
-		0,0,1,1,0,0,
-		0,0,0,1,0,0,
-		0,0,0,0,0,0
+	constexpr int toad2_1[16]{
+		0,1,0,0,
+		0,1,1,0,
+		0,1,1,0,
+		0,0,1,0
 	};
 
-	constexpr int toad2_2[36]{
-		0,0,0,0,0,0,
-		0,0,1,1,0,0,
-		0,1,0,0,0,0,
-		0,0,0,0,1,0,
-		0,0,1,1,0,0,
-		0,0,0,0,0,0
+	constexpr int toad2_2[16]{
+		0,1,1,0,
+		1,0,0,0,
+		0,0,0,1,
+		0,1,1,0
 	};
 
 }
